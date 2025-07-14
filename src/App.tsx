4d3bf7c56a72c0fd86c91f89794130627d21453e
Yago Morales Ares <yagomoralesares@gmail.com>
2025-07-14 21:31:35 +0200

    cleaning unnecessary router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import AppRouter from './Router'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AppRouter />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
